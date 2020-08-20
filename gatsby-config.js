3b22fa692bfc0de4a9d2433d9acbb6e8454122d0
reha <reha@youtubelearn.com>
2020-08-20 16:03:55 +0800

    feat: build a page with graphQL query

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,9 +10,10 @@ module.exports = {
   },
   plugins: [
     {
-      resolve: `gatsby-plugin-typography`,
+      resolve: `gatsby-source-filesystem`,
       options: {
-        pathToConfigModule: `src/utils/typography`,
+        name: `src`,
+        path: `${__dirname}/src/`,
       },
     },
     `gatsby-plugin-emotion`,
